cabf6abd6731ec53cd1c9acc3f2b3a3a6521cb0a
Artyom <seminartem98@mail.ru>
2020-12-27 18:30:42 +0700

    added simple structure of the projext and menu-item component with simple styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import HomePage from './components/homePage/HomePage';
+import HomePage from './pages/homePage/HomePage';
 
 function App() {
   return (
