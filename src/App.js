b1f4df846c5e2e5d4eb67f8c488eb50bd4a7c9ce
Artyom <seminartem98@mail.ru>
2020-12-29 13:58:45 +0700

    added some shop items for shop page

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import './App.css';
 import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/homePage/HomePage';
+import ShopPage from './pages/shopPage/ShopPage';
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={HomePage} />
-        {/* <Route path="/hats" components={HatsPage} /> */}
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
