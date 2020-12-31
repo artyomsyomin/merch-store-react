3f8caa47935cd71b06fdf18ab9d56fe07fee312f
Artyom <seminartem98@mail.ru>
2020-12-31 18:52:15 +0700

    added removing items from checkout page

--- src/redux/cart/cartActions.js
+++ src/redux/cart/cartActions.js
@@ -8,3 +8,8 @@ export const addItem = (item) => ({
   type: cartActionConstants.ADD_ITEM,
   payload: item,
 });
+
+export const removeItem = (item) => ({
+  type: cartActionConstants.CLEAR_ITEM_FROM_CART,
+  payload: item,
+});
