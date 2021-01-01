3b134d9648342871706a44eade85d79371f0812a
Artyom <seminartem98@mail.ru>
2021-01-01 13:19:07 +0700

    added directoryReducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux';
 
 import userReducer from './user/userReducer';
 import cartReducer from './cart/cartReducer';
+import directoryReducer from './directory/directoryReducer';
 
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
@@ -15,6 +16,7 @@ const persistConfig = {
 const rootReducer = combineReducers({
   userReducer,
   cartReducer,
+  directoryReducer,
 });
 
 export default persistReducer(persistConfig, rootReducer);
